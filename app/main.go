578386dba800108e764fda0e9ca662bb153d33ea
akshay.nambiar <akshay.nambiar@go-jek.com>
2021-02-10 21:04:00 +0530

    chang go mod

--- app/main.go
+++ app/main.go
@@ -1,7 +1,7 @@
 package main
 
 import "fmt"
-import "pkg_test/pkg/string"
+import "github.com/epsilon-akshay/pkg_example/pkg/string"
 func main() {
 	fmt.Print("hi there")
 	fmt.Print(string.Hash())
--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
-module pkg_example
+module github.com/epsilon-akshay/pkg_example
 
 go 1.15
-
-replace pkg_example => github.com/epsilon-akshay/pkg_example v0.0.0-20210210151055-e882b754b8ad
