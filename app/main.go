e882b754b8ad4055b7d3705b1471b2ef1d8ac049
akshay.nambiar <akshay.nambiar@go-jek.com>
2021-02-10 20:40:55 +0530

    add public method

---
+++ app/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+import "pkg_test/pkg/string"
+func main() {
+	fmt.Print("hi there")
+	fmt.Print(string.Hash())
+}
---
+++ pkg/string/hash.go
@@ -0,0 +1,5 @@
+package string
+
+func Hash() string{
+	return "asdasdas"
+}
